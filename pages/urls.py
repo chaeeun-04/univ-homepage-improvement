fb0d16ebfab0bd652f0f0c0bc86da518fb721e39
sunghee <tjdgml040908@stu.ac.kr>
2025-11-16 22:58:59 +0900

    스터디&공조전 매칭 ㅠ

--- pages/urls.py
+++ pages/urls.py
@@ -1,15 +1,29 @@
 from django.urls import path
+
 from . import views
 
+
+
 urlpatterns = [
-    path('', views.index, name='index'),
+
+path('', views.index, name='index'),
 
 
-    path('timetable/', views.timetable_view, name='master_timetable'),
-    path('graduation/', views.graduation_view, name='graduation_requirements'),
-    path('forms/', views.forms_view, name='forms_archive'),
-    path('cafeteria/', views.cafeteria_view, name='cafeteria_menu'),
-    path('matching/', views.matching, name='matching'),
 
-    path('track/<str:menu_name_pk>/', views.track_menu_click, name='track_click'),
+
+
+path('timetable/', views.timetable_view, name='master_timetable'),
+
+path('graduation/', views.graduation_view, name='graduation_requirements'),
+
+path('forms/', views.forms_view, name='forms_archive'),
+
+path('cafeteria/', views.cafeteria_view, name='cafeteria_menu'),
+
+path('matching/', views.matching, name='matching'),
+
+
+
+path('track/<str:menu_name_pk>/', views.track_menu_click, name='track_click'),
+
 ]
